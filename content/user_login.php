a2fa091c176d9706b4c5d66b4545213dc8092298
jekleine <jef.kleine@gmail.com>
2016-11-20 14:50:48 +0100

    opzet login, registratie en home pagina

---
+++ content/user_login.php
@@ -0,0 +1,12 @@
+<form class="form-horizontal" action='./function/user_login.php' method="POST">
+  <fieldset>
+    <div id="legend">
+      <legend class="">Login</legend>
+    </div>
+      <!-- Button -->
+      <div class="controls">
+        <button class="btn btn-success">Login</button>
+      </div>
+    </div>
+  </fieldset>
+</form>
--- index.php
+++ index.php
@@ -3,25 +3,43 @@
   <head>
     <meta charset="utf-8">
     <title>title</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="/Hanshow/js/bootstrap.js"></script>
+	<?php include 'config.php'; ?>
   </head>
   
   <body>
+  <a href="/Hanshow/index.php?p=registeren">Registreren</a>
+  <a href="/Hanshow/index.php?p=login">Login</a>
+  <!--header-->
+	
   <!--inhoud-->
+  <div class="container well well-sm">
+
     <p>
 	<?php
 	if(!isset($_GET["p"]))
 	{
-		print("startpagina");
 		//Startpagina
+		include (ROOT_DIR . '/content/home.php') ;
 	}
 	elseif($_GET["p"] == "registeren")
 	{
-		//registratie
-		include '/content/user_register.php' ;
+		//registratie pagina
+		include (ROOT_DIR . '/content/user_register.php') ;
+	}
+	elseif($_GET["p"] == "login")
+	{
+		//login pagina
+		include (ROOT_DIR . '/content/user_login.php') ;
 	}
 	?>
+	
+	  <!--footer-->
+	  
+	  
 	</p>
+	</div>
   </body>
 </html>
